5bb66236135b27bd0f3904ae7ea0e6c5de7ebcc1
max197a <max197aa@gmail.com>
2018-09-17 09:32:31 +0300

    add project

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
   //////////
   // Global variables
   //////////
@@ -10,7 +9,7 @@ $(document).ready(function(){
   ////////////
   // READY - triggered when PJAX DONE
   ////////////
-  function pageReady(){
+  function pageReady() {
     legacySupport();
     updateHeaderActiveClass();
     initHeaderScroll();
@@ -23,7 +22,7 @@ $(document).ready(function(){
     initValidations();
 
     // development helper
-    _window.on('resize', debounce(setBreakpoint, 200))
+    _window.on("resize", debounce(setBreakpoint, 200));
 
     // AVAILABLE in _components folder
     // copy paste in main.js and initialize here
@@ -38,18 +37,16 @@ $(document).ready(function(){
   // this is a master function which should have all functionality
   pageReady();
 
-
   // some plugins work best with onload triggers
-  _window.on('load', function(){
+  _window.on("load", function() {
     // your functions
-  })
-
+  });
 
   //////////
   // COMMON
   //////////
 
-  function legacySupport(){
+  function legacySupport() {
     // svg support for laggy browsers
     svg4everybody();
 
@@ -61,66 +58,93 @@ $(document).ready(function(){
     });
   }
 
-
   // Prevent # behavior
-	_document
-    .on('click', '[href="#"]', function(e) {
-  		e.preventDefault();
-  	})
-    .on('click', 'a[href^="#section"]', function() { // section scroll
-      var el = $(this).attr('href');
-      $('body, html').animate({
-          scrollTop: $(el).offset().top}, 1000);
-      return false;
+  _document
+    .on("click", '[href="#"]', function(e) {
+      e.preventDefault();
     })
-
+    .on("click", 'a[href^="#section"]', function() {
+      // section scroll
+      var el = $(this).attr("href");
+      $("body, html").animate(
+        {
+          scrollTop: $(el).offset().top - 100
+        },
+        1000
+      );
+      return false;
+    });
 
   // HEADER SCROLL
   // add .header-static for .page or body
   // to disable sticky header
-  function initHeaderScroll(){
-    _window.on('scroll', throttle(function(e) {
-      var vScroll = _window.scrollTop();
-      var header = $('.header').not('.header--static');
-      var headerHeight = header.height();
-      var firstSection = _document.find('.page__content div:first-child()').height() - headerHeight;
-      var visibleWhen = Math.round(_document.height() / _window.height()) >  2.5
+  function initHeaderScroll() {
+    _window.on(
+      "scroll",
+      throttle(function(e) {
+        var vScroll = _window.scrollTop();
+        var header = $(".header").not(".header--static");
+        var headerHeight = header.height();
+        var firstSection =
+          _document.find(".page__content div:first-child()").height() -
+          headerHeight;
+        var visibleWhen =
+          Math.round(_document.height() / _window.height()) > 2.5;
 
-      if (visibleWhen){
-        if ( vScroll > headerHeight ){
-          header.addClass('is-fixed');
-        } else {
-          header.removeClass('is-fixed');
+        if (visibleWhen) {
+          if (vScroll > headerHeight) {
+            header.addClass("is-fixed");
+          } else {
+            header.removeClass("is-fixed");
+          }
+          if (vScroll > firstSection) {
+            header.addClass("is-fixed-visible");
+          } else {
+            header.removeClass("is-fixed-visible");
+          }
         }
-        if ( vScroll > firstSection ){
-          header.addClass('is-fixed-visible');
-        } else {
-          header.removeClass('is-fixed-visible');
-        }
-      }
-    }, 10));
+      }, 10)
+    );
   }
 
-
   // HAMBURGER TOGGLER
-  _document.on('click', '[js-hamburger]', function(){
-    $(this).toggleClass('is-active');
-    $('.mobile-navi').toggleClass('is-active');
+  _document.on("click", "[js-hamburger]", function() {
+    $(this).toggleClass("is-active");
+    $(".mobile-navi").toggleClass("is-active");
+  });
+  ////////////////////
+  // FORM TOGGLER
+  ////////////////////
+
+  _document.on("click", "[open-form]", function() {
+    $(".form-block-hidden").slideToggle();
   });
 
-  function closeMobileMenu(){
-    $('[js-hamburger]').removeClass('is-active');
-    $('.mobile-navi').removeClass('is-active');
+  _document.on("click", "[close-form]", function() {
+    $(".form-block-hidden").slideToggle();
+  });
+
+  ////////////////////
+  // FORM TOGGLER
+  ////////////////////
+
+  function closeMobileMenu() {
+    $("[js-hamburger]").removeClass("is-active");
+    $(".mobile-navi").removeClass("is-active");
   }
 
   // SET ACTIVE CLASS IN HEADER
   // * could be removed in production and server side rendering when header is inside barba-container
-  function updateHeaderActiveClass(){
-    $('.header__menu li').each(function(i,val){
-      if ( $(val).find('a').attr('href') == window.location.pathname.split('/').pop() ){
-        $(val).addClass('is-active');
+  function updateHeaderActiveClass() {
+    $(".header__menu li").each(function(i, val) {
+      if (
+        $(val)
+          .find("a")
+          .attr("href") == window.location.pathname.split("/").pop()
+      ) {
+        $(val).addClass("is-active");
       } else {
-        $(val).removeClass('is-active')
+        $(val).removeClass("is-active");
       }
     });
   }
@@ -129,29 +153,28 @@ $(document).ready(function(){
   // SLIDERS
   //////////
 
-  function initSliders(){
-
+  function initSliders() {
     // EXAMPLE SWIPER
-    new Swiper('[js-slider]', {
+    new Swiper("[js-slider]", {
       wrapperClass: "swiper-wrapper",
       slideClass: "example-slide",
-      direction: 'horizontal',
+      direction: "horizontal",
       loop: false,
       watchOverflow: true,
       setWrapperSize: false,
       spaceBetween: 0,
-      slidesPerView: 'auto',
+      slidesPerView: "auto",
       // loop: true,
       normalizeSlideIndex: true,
       // centeredSlides: true,
       freeMode: true,
       // effect: 'fade',
       autoplay: {
-        delay: 5000,
+        delay: 5000
       },
       navigation: {
-        nextEl: '.example-next',
-        prevEl: '.example-prev',
+        nextEl: ".example-next",
+        prevEl: ".example-prev"
       },
       breakpoints: {
         // when window width is <= 992px
@@ -159,27 +182,26 @@ $(document).ready(function(){
           autoHeight: true
         }
       }
-    })
-
+    });
   }
 
   //////////
   // MODALS
   //////////
 
-  function initPopups(){
+  function initPopups() {
     // Magnific Popup
     var startWindowScroll = 0;
-    $('[js-popup]').magnificPopup({
-      type: 'inline',
+    $("[js-popup]").magnificPopup({
+      type: "inline",
       fixedContentPos: true,
       fixedBgPos: true,
-      overflowY: 'auto',
+      overflowY: "auto",
       closeBtnInside: true,
       preloader: false,
       midClick: true,
       removalDelay: 300,
-      mainClass: 'popup-buble',
+      mainClass: "popup-buble",
       callbacks: {
         beforeOpen: function() {
           startWindowScroll = _window.scrollTop();
@@ -192,23 +214,23 @@ $(document).ready(function(){
       }
     });
 
-    $('[js-popup-gallery]').magnificPopup({
-  		delegate: 'a',
-  		type: 'image',
-  		tLoading: 'Загрузка #%curr%...',
-  		mainClass: 'popup-buble',
-  		gallery: {
-  			enabled: true,
-  			navigateByImgClick: true,
-  			preload: [0,1]
-  		},
-  		image: {
-  			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-  		}
-  	});
+    $("[js-popup-gallery]").magnificPopup({
+      delegate: "a",
+      type: "image",
+      tLoading: "Загрузка #%curr%...",
+      mainClass: "popup-buble",
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1]
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+      }
+    });
   }
 
-  function closeMfp(){
+  function closeMfp() {
     $.magnificPopup.close();
   }
 
@@ -218,81 +240,71 @@ $(document).ready(function(){
 
   // textarea autoExpand
   _document
-    .one('focus.autoExpand', '.ui-group textarea', function(){
-        var savedValue = this.value;
-        this.value = '';
-        this.baseScrollHeight = this.scrollHeight;
-        this.value = savedValue;
+    .one("focus.autoExpand", ".ui-group textarea", function() {
+      var savedValue = this.value;
+      this.value = "";
+      this.baseScrollHeight = this.scrollHeight;
+      this.value = savedValue;
     })
-    .on('input.autoExpand', '.ui-group textarea', function(){
-        var minRows = this.getAttribute('data-min-rows')|0, rows;
-        this.rows = minRows;
-        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
-        this.rows = minRows + rows;
+    .on("input.autoExpand", ".ui-group textarea", function() {
+      var minRows = this.getAttribute("data-min-rows") | 0,
+        rows;
+      this.rows = minRows;
+      rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
+      this.rows = minRows + rows;
     });
 
   // Masked input
-  function initMasks(){
-    $("[js-dateMask]").mask("99.99.99",{placeholder:"ДД.ММ.ГГ"});
-    $("input[type='tel']").mask("+7 (000) 000-0000", {placeholder: "+7 (___) ___-____"});
+  function initMasks() {
+    $("[js-dateMask]").mask("99.99.99", { placeholder: "ДД.ММ.ГГ" });
+    $("input[type='tel']").mask("+7 (000) 000-0000", {
+      placeholder: "+7 (___) ___-____"
+    });
   }
 
   // selectric
-  function initSelectric(){
-    $('select').selectric({
+  function initSelectric() {
+    $("select").selectric({
       maxHeight: 300,
-      arrowButtonMarkup: '<b class="button"><svg class="ico ico-select-down"><use xlink:href="img/sprite.svg#ico-select-down"></use></svg></b>',
-
-      onInit: function(element, data){
-        var $elm = $(element),
-            $wrapper = $elm.closest('.' + data.classes.wrapper);
-
-        $wrapper.find('.label').html($elm.attr('placeholder'));
-      },
-      onBeforeOpen: function(element, data){
-        var $elm = $(element),
-            $wrapper = $elm.closest('.' + data.classes.wrapper);
-
-        $wrapper.find('.label').data('value', $wrapper.find('.label').html()).html($elm.attr('placeholder'));
-      },
-      onBeforeClose: function(element, data){
-        var $elm = $(element),
-            $wrapper = $elm.closest('.' + data.classes.wrapper);
-
-        $wrapper.find('.label').html($wrapper.find('.label').data('value'));
-      }
+      disableOnMobile: false,
+      nativeOnMobile: false
     });
   }
 
   ////////////
   // SCROLLMONITOR - WOW LIKE
   ////////////
-  function initScrollMonitor(){
-    $('.wow').each(function(i, el){
-
-      var elWatcher = scrollMonitor.create( $(el) );
+  function initScrollMonitor() {
+    $(".wow").each(function(i, el) {
+      var elWatcher = scrollMonitor.create($(el));
 
       var delay;
-      if ( $(window).width() < 768 ){
-        delay = 0
+      if ($(window).width() < 768) {
+        delay = 0;
       } else {
-        delay = $(el).data('animation-delay');
+        delay = $(el).data("animation-delay");
       }
 
-      var animationClass = $(el).data('animation-class') || "wowFadeUp"
+      var animationClass = $(el).data("animation-class") || "wowFadeUp";
 
-      var animationName = $(el).data('animation-name') || "wowFade"
+      var animationName = $(el).data("animation-name") || "wowFade";
 
-      elWatcher.enterViewport(throttle(function() {
-        $(el).addClass(animationClass);
-        $(el).css({
-          'animation-name': animationName,
-          'animation-delay': delay,
-          'visibility': 'visible'
-        });
-      }, 100, {
-        'leading': true
-      }));
+      elWatcher.enterViewport(
+        throttle(
+          function() {
+            $(el).addClass(animationClass);
+            $(el).css({
+              "animation-name": animationName,
+              "animation-delay": delay,
+              visibility: "visible"
+            });
+          },
+          100,
+          {
+            leading: true
+          }
+        )
+      );
       // elWatcher.exitViewport(throttle(function() {
       //   $(el).removeClass(animationClass);
       //   $(el).css({
@@ -302,7 +314,6 @@ $(document).ready(function(){
       //   });
       // }, 100));
     });
-
   }
 
   ////////////////
@@ -311,53 +322,59 @@ $(document).ready(function(){
 
   // jQuery validate plugin
   // https://jqueryvalidation.org
-  function initValidations(){
+  function initValidations() {
     // GENERIC FUNCTIONS
     var validateErrorPlacement = function(error, element) {
-      error.addClass('ui-input__validation');
+      error.addClass("ui-input__validation");
       error.appendTo(element.parent("div"));
-    }
+    };
     var validateHighlight = function(element) {
-      $(element).parent('div').addClass("has-error");
-    }
+      $(element)
+        .parent("div")
+        .addClass("has-error");
+    };
     var validateUnhighlight = function(element) {
-      $(element).parent('div').removeClass("has-error");
-    }
+      $(element)
+        .parent("div")
+        .removeClass("has-error");
+    };
     var validateSubmitHandler = function(form) {
-      $(form).addClass('loading');
+      $(form).addClass("loading");
       $.ajax({
         type: "POST",
-        url: $(form).attr('action'),
+        url: $(form).attr("action"),
         data: $(form).serialize(),
         success: function(response) {
-          $(form).removeClass('loading');
+          $(form).removeClass("loading");
           var data = $.parseJSON(response);
-          if (data.status == 'success') {
+          if (data.status == "success") {
             // do something I can't test
           } else {
-              $(form).find('[data-error]').html(data.message).show();
+            $(form)
+              .find("[data-error]")
+              .html(data.message)
+              .show();
           }
         }
       });
-    }
+    };
 
     var validatePhone = {
       required: true,
       normalizer: function(value) {
-          var PHONE_MASK = '+X (XXX) XXX-XXXX';
-          if (!value || value === PHONE_MASK) {
-              return value;
-          } else {
-              return value.replace(/[^\d]/g, '');
-          }
+        var PHONE_MASK = "+X (XXX) XXX-XXXX";
+        if (!value || value === PHONE_MASK) {
+          return value;
+        } else {
+          return value.replace(/[^\d]/g, "");
+        }
       },
       minlength: 11,
       digits: true
-    }
+    };
 
     ////////
     // FORMS
-
 
     /////////////////////
     // REGISTRATION FORM
@@ -376,7 +393,7 @@ $(document).ready(function(){
         },
         password: {
           required: true,
-          minlength: 6,
+          minlength: 6
         }
         // phone: validatePhone
       },
@@ -384,13 +401,13 @@ $(document).ready(function(){
         last_name: "Заполните это поле",
         first_name: "Заполните это поле",
         email: {
-            required: "Заполните это поле",
-            email: "Email содержит неправильный формат"
+          required: "Заполните это поле",
+          email: "Email содержит неправильный формат"
         },
         password: {
-            required: "Заполните это поле",
-            email: "Пароль мимимум 6 символов"
-        },
+          required: "Заполните это поле",
+          email: "Пароль мимимум 6 символов"
+        }
         // phone: {
         //     required: "Заполните это поле",
         //     minlength: "Введите корректный телефон"
@@ -399,19 +416,18 @@ $(document).ready(function(){
     });
   }
 
-
   //////////
   // BARBA PJAX
   //////////
-  var easingSwing = [.02, .01, .47, 1]; // default jQuery easing for anime.js
+  var easingSwing = [0.02, 0.01, 0.47, 1]; // default jQuery easing for anime.js
 
   Barba.Pjax.Dom.containerClass = "page";
 
   var FadeTransition = Barba.BaseTransition.extend({
     start: function() {
-      Promise
-        .all([this.newContainerLoading, this.fadeOut()])
-        .then(this.fadeIn.bind(this));
+      Promise.all([this.newContainerLoading, this.fadeOut()]).then(
+        this.fadeIn.bind(this)
+      );
     },
 
     fadeOut: function() {
@@ -419,15 +435,15 @@ $(document).ready(function(){
 
       anime({
         targets: this.oldContainer,
-        opacity : .5,
+        opacity: 0.5,
         easing: easingSwing, // swing
         duration: 300,
-        complete: function(anim){
+        complete: function(anim) {
           deferred.resolve();
         }
-      })
+      });
 
-      return deferred.promise
+      return deferred.promise;
     },
 
     fadeIn: function() {
@@ -437,8 +453,8 @@ $(document).ready(function(){
       $(this.oldContainer).hide();
 
       $el.css({
-        visibility : 'visible',
-        opacity : .5
+        visibility: "visible",
+        opacity: 0.5
       });
 
       anime({
@@ -454,7 +470,7 @@ $(document).ready(function(){
         easing: easingSwing, // swing
         duration: 300,
         complete: function(anim) {
-          triggerBody()
+          triggerBody();
           _this.done();
         }
       });
@@ -469,15 +485,18 @@ $(document).ready(function(){
   Barba.Prefetch.init();
   Barba.Pjax.start();
 
-  Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container, newPageRawHTML) {
-
+  Barba.Dispatcher.on("newPageReady", function(
+    currentStatus,
+    oldStatus,
+    container,
+    newPageRawHTML
+  ) {
     pageReady();
     closeMobileMenu();
-
   });
 
   // some plugins get bindings onNewPage only that way
-  function triggerBody(){
+  function triggerBody() {
     _window.scrollTop(0);
     $(window).scroll();
     $(window).resize();
@@ -486,7 +505,7 @@ $(document).ready(function(){
   //////////
   // MEDIA Condition helper function
   //////////
-  function mediaCondition(cond){
+  function mediaCondition(cond) {
     var disabledBp;
     var conditionMedia = cond.substring(1);
     var conditionPosition = cond.substring(0, 1);
@@ -497,28 +516,28 @@ $(document).ready(function(){
       disabledBp = _window.width() > conditionMedia;
     }
 
-    return disabledBp
+    return disabledBp;
   }
 
   //////////
   // DEVELOPMENT HELPER
   //////////
-  function setBreakpoint(){
-    var wHost = window.location.host.toLowerCase()
-    var displayCondition = wHost.indexOf("localhost") >= 0 || wHost.indexOf("surge") >= 0
-    if (displayCondition){
+  function setBreakpoint() {
+    var wHost = window.location.host.toLowerCase();
+    var displayCondition =
+      wHost.indexOf("localhost") >= 0 || wHost.indexOf("surge") >= 0;
+    if (displayCondition) {
       var wWidth = _window.width();
 
-      var content = "<div class='dev-bp-debug'>"+wWidth+"</div>";
+      var content = "<div class='dev-bp-debug'>" + wWidth + "</div>";
 
-      $('.page').append(content);
-      setTimeout(function(){
-        $('.dev-bp-debug').fadeOut();
-      },1000);
-      setTimeout(function(){
-        $('.dev-bp-debug').remove();
-      },1500)
+      $(".page").append(content);
+      setTimeout(function() {
+        $(".dev-bp-debug").fadeOut();
+      }, 1000);
+      setTimeout(function() {
+        $(".dev-bp-debug").remove();
+      }, 1500);
     }
   }
-
 });
